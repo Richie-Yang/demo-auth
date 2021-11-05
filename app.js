379056cdd732a82e5b34ef156b64d7c94fa1e39c
Richie-Yang <richieyang8813@gmail.com>
2021-11-05 16:03:38 +0800

    feat: add Express-Handlebars + login page

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const PORT = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
 })
 
 

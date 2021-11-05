ecd0fba3523c069a173031c8b54c4a2ffac5bf51
Richie-Yang <richieyang8813@gmail.com>
2021-11-05 16:13:07 +0800

    feat: add body-parser and login form + login route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
@@ -14,6 +17,11 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  console.log(email, password)
 })
 
 

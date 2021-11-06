ac9ebeb21c0b99f3f8d8942451490c465a6e3484
Richie-Yang <richieyang8813@gmail.com>
2021-11-06 13:54:22 +0800

    fix: modify userSeeder.js

--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -3,16 +3,24 @@ const User = require('../user')
 const dummyData = require('../../sample_users')
 
 
+// import dummy data one by one
 db.once('open', () => {
   dummyData.forEach(({ firstName, email, password }) => {
     // import dummy data
     User.create({first_name: firstName, email, password})
-      .then(() => {
-        console.log('userSeeder done!')
-        // close DB connection afterward
-        db.close()
-      })
       .catch(error => console.log(error))
-      .finally(() => process.exit()) // exit the process anyway
   })
+  console.log('userSeeder done!')
 })
+
+
+// import all dummy data at once
+// db.once('open', () => {
+//   User.create(dummyData)
+//     .then(() => {
+//       console.log('userSeeder done!')
+//       db.close()
+//     })
+//     .catch(error => console.log(error))
+//     .finally(() => process.exit())
+// })
